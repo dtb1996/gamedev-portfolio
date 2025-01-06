031c0fd647cefdb8904e149ff455867522b4255f
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-01-06 18:27:19 -0500

    Updated site metadata and contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,37 +3,35 @@
     
     <h1>Let's get in touch</h1>
 
-    <div style="margin-bottom:40px;">I'm looking for a job !<br/>Feel free to contact me about any opportunity, or just to chat about cooking, kittens or anything else.</div>
+    <div style="margin-bottom:40px;">Feel free to contact me about any opportunity.</div>
 
     <ul>
       <li>
         <i class="fa fa-user-circle-o fa-lg fa-fw"></i>
-        <a href="http://mywebsite.com" target="_blank">https://mywebsite.com</a>
+        <a href="https://dillonbellefeuille.com/" target="_blank">dillonbellefeuille.com</a>
       </li>
       <li>
         <i class="fa fa-envelope-o fa-lg fa-fw"></i>
-        <a href="mailto:my@email.com" target="_blank">my@email.com</a>
+        <a href="mailto:bellefeuilledillon@gmail.com" target="_blank">bellefeuilledillon@gmail.com</a>
       </li>
       <li>
         <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
-        <a href="https://www.linkedin.com/me" target="_blank">linkedin.com/me</a>
+        <a href="https://www.linkedin.com/in/dillon-bellefeuille/" target="_blank">linkedin.com/in/dillon-bellefeuille</a>
       </li>
       <li>
         <i class="fa fa-github fa-lg fa-fw"></i>
-        <a href="https://github.com/me" target="_blank">github.com/me</a>
+        <a href="https://github.com/dtb1996" target="_blank">github.com/dtb1996</a>
       </li>
       <li>
         <i class="fa fa-gamepad fa-lg fa-fw"></i>
-        <a href="https://myself.itch.io" target="_blank">myself.itch.io</a>
+        <a href="https://dillionaire.itch.io/" target="_blank">dillionaire.itch.io</a>
       </li>
-      <li>
-        <i class="fa fa-tumblr fa-lg fa-fw"></i>
-        <a href="https://myblog.tumblr.com" target="_blank">myblog.tumblr.com</a>
-      </li>
+      <!--
       <li>
         <i class="fa fa-steam fa-lg fa-fw"></i>
         <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
       </li>
+      -->
     </ul>
 
   </div>
